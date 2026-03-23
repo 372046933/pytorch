eaf850bd54e7fe39ee1dd6603de7d5bcd1dc543a
372046933 <wondertx@gmail.com>
2026-03-23 14:46:36 +0800

    Fix gcc build

--- aten/src/ATen/core/IListRef_inl.h
+++ aten/src/ATen/core/IListRef_inl.h
@@ -168,7 +168,11 @@ class IListRefTagImpl<IListRefTag::Boxed, at::OptionalTensorRef>
    */
   static IListRefConstRef<at::OptionalTensorRef> iterator_get(
       const typename list_type::const_iterator& it) {
+#if defined(__clang__)
+    C10_DIAGNOSTIC_PUSH_AND_IGNORED_IF_DEFINED("-Wdangling")
+#else
     C10_DIAGNOSTIC_PUSH_AND_IGNORED_IF_DEFINED("-Wdangling-reference")
+#endif
     const auto& ivalue = (*it).get();
     C10_DIAGNOSTIC_POP()
     if (!ivalue.isNone()) {
